729a804a7c667ff96fb4687ac6dfd7871c46c2bb
Lee <ljs0429777@gmail.com>
2023-01-25 17:03:03 +0900

    fix : camel case로 메소드 네이밍 변경

--- src/main/java/com/jeongseok/board/repository/UserRepository.java
+++ src/main/java/com/jeongseok/board/repository/UserRepository.java
@@ -3,11 +3,9 @@ package com.jeongseok.board.repository;
 import com.jeongseok.board.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	Optional<User> findByUserName(String userName);
+	Optional<User> findByUsername(String userName);
 
 }
--- src/main/java/com/jeongseok/board/service/UserService.java
+++ src/main/java/com/jeongseok/board/service/UserService.java
@@ -2,6 +2,7 @@ package com.jeongseok.board.service;
 
 import com.jeongseok.board.model.UserDto;
 import com.jeongseok.board.repository.UserRepository;
+import com.jeongseok.board.type.Role;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,7 @@ public class UserService {
 	@Transactional
 	public Long join(UserDto userDto) {
 		userDto.setPassword(encoder.encode(userDto.getPassword()));
+		userDto.setRole(Role.USER);
 
 		return userRepository.save(userDto.toEntity()).getId();
 	}
