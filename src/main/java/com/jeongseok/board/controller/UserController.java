6c9a6db6f64071eb6e3901934d1b7315f24f0590
Lee <ljs0429777@gmail.com>
2023-02-01 15:40:08 +0900

    feat : 로그인 후 **님 환영합니다 기능 추가, 로그아웃 기능 추가

--- src/main/java/com/jeongseok/board/controller/UserController.java
+++ src/main/java/com/jeongseok/board/controller/UserController.java
@@ -2,9 +2,11 @@ package com.jeongseok.board.controller;
 
 import com.jeongseok.board.dto.CreateUserDto;
 import com.jeongseok.board.service.UserService;
+import java.util.Map;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,11 +38,17 @@ public class UserController {
 	}
 
 	@PostMapping("/auth/join")
-	public String join(@Valid CreateUserDto createUserDto, Errors errors) {
+	public String join(@Valid CreateUserDto createUserDto, Errors errors, Model model) {
 
 		if (errors.hasErrors()) {
-//			return "/user/join";
+			/* 유효성 통과 못한 필드와 메시지를 핸들링 */
+			Map<String, String> validatorResult = userService.validateHandling(errors);
+			for (String key : validatorResult.keySet()) {
+				model.addAttribute(key, validatorResult.get(key));
+			}
+			return "/user/join";
 		}
+
 		userService.join(createUserDto);
 
 		return "redirect:/login";
--- src/main/java/com/jeongseok/board/service/UserService.java
+++ src/main/java/com/jeongseok/board/service/UserService.java
@@ -3,8 +3,12 @@ package com.jeongseok.board.service;
 import com.jeongseok.board.domain.User;
 import com.jeongseok.board.dto.CreateUserDto;
 import com.jeongseok.board.dto.CustomUserDetails;
+import com.jeongseok.board.dto.UserDto;
 import com.jeongseok.board.repository.UserRepository;
 import com.jeongseok.board.type.UserRole;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +16,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
 @RequiredArgsConstructor
 @Component
@@ -20,12 +26,8 @@ public class UserService implements UserDetailsService {
 	private final UserRepository userRepository;
 
 	private final BCryptPasswordEncoder encoder;
+	private final HttpSession session;
 
-	/**
-	 *
-	 * @param createUserDto
-	 * @return
-	 */
 	@Transactional
 	public Long join(CreateUserDto createUserDto) {
 		createUserDto.setPassword(encoder.encode(createUserDto.getPassword()));
@@ -39,6 +41,19 @@ public class UserService implements UserDetailsService {
 		User user = userRepository.findByUsername(username).orElseThrow(() ->
 			new UsernameNotFoundException("해당 사용자가 존재하지 않습니다. : " + username));
 
+		session.setAttribute("user", UserDto.Response.from(user));
+
 		return new CustomUserDetails(user);
+	}
+
+	public Map<String, String> validateHandling(Errors errors) {
+		Map<String, String> validatorResult = new HashMap<>();
+
+		/* 유효성 검사에 실패한 필드 목록을 받음 */
+		for (FieldError error : errors.getFieldErrors()) {
+			String validKeyName = String.format("valid_%s", error.getField());
+			validatorResult.put(validKeyName, error.getDefaultMessage());
+		}
+		return validatorResult;
 	}
 }
